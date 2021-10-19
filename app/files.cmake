33bd07970dce9fe59e871b2d36f8fd5159266330
jansa2708 <55147672+jansa2708@users.noreply.github.com>
2021-10-19 12:18:33 +0200

    Initial Commit

---
+++ app/files.cmake
@@ -0,0 +1,7 @@
+set(HEADERS
+	${CMAKE_CURRENT_LIST_DIR}/include/Main.h
+)
+
+set(CPPS
+	${CMAKE_CURRENT_LIST_DIR}/source/Main.cpp
+)
---
+++ libs/0-Plugins/files.cmake
@@ -0,0 +1,7 @@
+set(HEADERS
+	#${CMAKE_CURRENT_LIST_DIR}/include/Header.h
+)
+
+set(CPPS
+	#${CMAKE_CURRENT_LIST_DIR}/source/Cpp.cpp
+)
