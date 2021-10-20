79150e9163b9d381de59ad049574633b588bd879
jansa2708 <j.sahrhage@dkfz.de>
2021-10-20 14:32:12 +0200

    Add AbstractionLayer for logic of PlayingCards

--- libs/3-Domain/files.cmake
+++ libs/3-Domain/files.cmake
@@ -1,7 +1,9 @@
 set(HEADERS
 	${CMAKE_CURRENT_LIST_DIR}/include/Player.h
+	${CMAKE_CURRENT_LIST_DIR}/include/Bet.h
 )
 
 set(CPPS
 	${CMAKE_CURRENT_LIST_DIR}/source/Player.cpp
+	${CMAKE_CURRENT_LIST_DIR}/source/Bet.cpp
 )
---
+++ libs/4-Abstraction/files.cmake
@@ -0,0 +1,7 @@
+set(HEADERS
+	# ${CMAKE_CURRENT_LIST_DIR}/include/Player.h
+)
+
+set(CPPS
+	# ${CMAKE_CURRENT_LIST_DIR}/source/Player.cpp
+)
