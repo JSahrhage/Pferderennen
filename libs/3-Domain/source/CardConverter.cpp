be4a79096eb775fd6553375dbda8dbe7d0c69dda
Jan <jan-sahrhage@gmx.de>
2022-04-29 19:50:25 +0200

    Replace Switch with If/ElseIf-Construct

--- libs/3-Domain/source/CardConverter.cpp
+++ libs/3-Domain/source/CardConverter.cpp
@@ -7,105 +7,77 @@ namespace Card
     {
         std::string acronym;
 
-        switch (card.getPip())
+        Pip cardPip = card.getPip();
+        Suit cardSuit = card.getSuit();
+
+        if (cardPip == Pip::Ace)
         {
-            case Pip::Ace:
-            {
-                acronym.append("A");
-                break;
-            }
-            case Pip::Two:
-            {
-                acronym.append("2");
-                break;
-            }
-            case Pip::Three:
-            {
-                acronym.append("3");
-                break;
-            }
-            case Pip::Four:
-            {
-                acronym.append("4");
-                break;
-            }
-            case Pip::Five:
-            {
-                acronym.append("5");
-                break;
-            }
-            case Pip::Six:
-            {
-                acronym.append("6");
-                break;
-            }
-            case Pip::Seven:
-            {
-                acronym.append("7");
-                break;
-            }
-            case Pip::Eight:
-            {
-                acronym.append("8");
-                break;
-            }
-            case Pip::Nine:
-            {
-                acronym.append("9");
-                break;
-            }
-            case Pip::Ten:
-            {
-                acronym.append("10");
-                break;
-            }
-            case Pip::Jack:
-            {
-                acronym.append("J");
-                break;
-            }
-            case Pip::Queen:
-            {
-                acronym.append("Q");
-                break;
-            }
-            case Pip::King:
-            {
-                acronym.append("K");
-                break;
-            }
-            default:
-            {
-                acronym.append("0");
-            }
+            acronym.append("A");
+        }
+        else if (cardPip == Pip::Two)
+        {
+            acronym.append("2");
+        }
+        else if (cardPip == Pip::Three)
+        {
+            acronym.append("3");
+        }
+        else if (cardPip == Pip::Four)
+        {
+            acronym.append("4");
+        }
+        else if (cardPip == Pip::Five)
+        {
+            acronym.append("5");
+        }
+        else if (cardPip == Pip::Six)
+        {
+            acronym.append("6");
+        }
+        else if (cardPip == Pip::Seven)
+        {
+            acronym.append("7");
+        }
+        else if (cardPip == Pip::Eight)
+        {
+            acronym.append("8");
+        }
+        else if (cardPip == Pip::Nine)
+        {
+            acronym.append("9");
+        }
+        else if (cardPip == Pip::Ten)
+        {
+            acronym.append("10");
+        }
+        else if (cardPip == Pip::Jack)
+        {
+            acronym.append("J");
+        }
+        else if (cardPip == Pip::Queen)
+        {
+            acronym.append("Q");
+        }
+        else if (cardPip == Pip::King)
+        {
+            acronym.append("K");
         }
 
-        switch (card.getSuit())
+        if (cardSuit == Suit::Diamond)
         {
-            case Suit::Diamond:
-            {
-                acronym.append("D");
-                break;
-            }
-            case Suit::Heart:
-            {
-                acronym.append("H");
-                break;
-            }
-            case Suit::Spade:
-            {
-                acronym.append("S");
-                break;
-            }
-            case Suit::Club:
-            {
-                acronym.append("C");
-                break;
-            }
-            default:
-            {
-                acronym.append("");
-            }
+            acronym.append("D");
+        }
+        else if (cardSuit == Suit::Heart)
+        {
+            acronym.append("H");
+        }
+        else if (cardSuit == Suit::Spade)
+        {
+            acronym.append("S");
+        }
+        else if (cardSuit == Suit::Club)
+        {
+            acronym.append("C");
         }
 
         return acronym;
